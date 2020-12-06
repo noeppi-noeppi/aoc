c45a834f97ece15bb0548f616adfb2166356341f
noeppi_noeppi <noeppinoeppi@gmail.com>
2020-12-06 11:10:26 +0100

    Day 6 - Add space

--- 2020/day6/part1.hs
+++ 2020/day6/part1.hs
@@ -5,7 +5,7 @@ main = do
     putStrLn (show (act input))
 
 nonEmpty a b = not (null a) && not (null b)
-join list = foldr (\a b-> a ++ b) "" list
+join list = foldr (\a b -> a ++ b) "" list
 
 act :: String -> Int
 act input = sum (map (length . nub . join) (filter (not . any null) (groupBy nonEmpty (lines input))))
