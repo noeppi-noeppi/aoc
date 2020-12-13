8d5fc635a471d02d36ee2afc44f9f02f5fc3e6c5
noeppi_noeppi <noeppinoeppi@gmail.com>
2020-12-13 15:19:59 +0100

    Remove stupid tabs

--- 2020/day13/part1.go
+++ 2020/day13/part1.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	line, _ := reader.ReadString('\n')
-	timestamp, _ := strconv.Atoi(strings.TrimSpace(line))
+    reader := bufio.NewReader(os.Stdin)
+    line, _ := reader.ReadString('\n')
+    timestamp, _ := strconv.Atoi(strings.TrimSpace(line))
 
-	line, _ = reader.ReadString('\n')
-	var ids = strings.Split(line, ",")
+    line, _ = reader.ReadString('\n')
+    var ids = strings.Split(line, ",")
 
-	var first = int(^uint(0) >> 1)
-	var bus = 0
+    var first = int(^uint(0) >> 1)
+    var bus = 0
 
-	for _, idStr := range ids {
-		if idStr != "x" {
-			id, _ := strconv.Atoi(strings.TrimSpace(idStr))
-			busIdx := timestamp / id
-			if timestamp % id != 0 {
-				busIdx += 1
-			}
-			busStamp := busIdx * id
-			if busStamp < first {
-				first = busStamp
-				bus = id
-			}
-		}
-	}
+    for _, idStr := range ids {
+        if idStr != "x" {
+            id, _ := strconv.Atoi(strings.TrimSpace(idStr))
+            busIdx := timestamp / id
+            if timestamp % id != 0 {
+                busIdx += 1
+            }
+            busStamp := busIdx * id
+            if busStamp < first {
+                first = busStamp
+                bus = id
+            }
+        }
+    }
 
-	fmt.Println((first - timestamp) * bus)
+    fmt.Println((first - timestamp) * bus)
 }
--- 2020/day13/part2.go
+++ 2020/day13/part2.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	_, _ = reader.ReadString('\n')
+    reader := bufio.NewReader(os.Stdin)
+    _, _ = reader.ReadString('\n')
 
-	line, _ := reader.ReadString('\n')
-	var idStrs = strings.Split(line, ",")
-	var ids = make([]int, len(idStrs))
-	for idx, idStr := range idStrs {
-		if idStr == "x" {
-			ids[idx] = -1
-		} else {
-			ids[idx], _ = strconv.Atoi(strings.TrimSpace(idStr))
-		}
-	}
+    line, _ := reader.ReadString('\n')
+    var idStrs = strings.Split(line, ",")
+    var ids = make([]int, len(idStrs))
+    for idx, idStr := range idStrs {
+        if idStr == "x" {
+            ids[idx] = -1
+        } else {
+            ids[idx], _ = strconv.Atoi(strings.TrimSpace(idStr))
+        }
+    }
 
-	minute := 0
-	step := 1
+    minute := 0
+    step := 1
 
-	for idx, id := range ids {
-		if id >= 0 {
-			for (minute + idx) % id != 0 {
-				minute += step
-			}
-			step = lcm(step, id)
-		}
-	}
+    for idx, id := range ids {
+        if id >= 0 {
+            for (minute + idx) % id != 0 {
+                minute += step
+            }
+            step = lcm(step, id)
+        }
+    }
 
-	fmt.Println(minute)
+    fmt.Println(minute)
 }
 
 func gcd(a int, b int) int {
-	for b != 0 {
-		t := b
-		b = a % b
-		a = t
-	}
-	return a
+    for b != 0 {
+        t := b
+        b = a % b
+        a = t
+    }
+    return a
 }
 
 func lcm(a int, b int) int {
-	return a * b / gcd(a, b)
+    return a * b / gcd(a, b)
 }
